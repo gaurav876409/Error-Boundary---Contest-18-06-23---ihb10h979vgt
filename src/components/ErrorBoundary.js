3b4da38fc3e37fa184698ed0924e35ce3e6d762c
gaurav876409 <gaurav876409@gmail.com>
2023-06-18 20:12:29 +0530

    up

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
- 
+  constructor(props){
+    super(props);
+    this.state = {error: false};
+  }
+  getDerivedStateFromError(error){
+    return {hasError: true};
+  }
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          
+          There is an error
         </p>
       );
     }
